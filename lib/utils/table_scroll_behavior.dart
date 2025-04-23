1e97502c0562111173b8bbd3b3b8f32a21888aba
Amrit Dash <amrit.dash60@gmail.com>
2025-04-23 13:23:56 +0530

    fix: entire pagescroll behavior

--- lib/utils/table_scroll_behavior.dart
+++ lib/utils/table_scroll_behavior.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:universal_html/html.dart' as html;
 
 class TableScrollBehavior extends ScrollBehavior {
   const TableScrollBehavior();
@@ -18,44 +17,14 @@ class TableScrollBehavior extends ScrollBehavior {
     return const ClampingScrollPhysics();
   }
 
+  //Required
+  bool shouldUpdateScrollBehavior(ScrollBehavior oldDelegate) => false;
+
   @override
   Widget buildScrollbar(
       BuildContext context, Widget child, ScrollableDetails details) {
-    // Prevent browser back/forward navigation when scrolling horizontally
-    return Listener(
-      onPointerSignal: (event) {
-        if (event is PointerScrollEvent) {
-          if (event.scrollDelta.dx != 0) {
-            html.window.history.pushState(null, '', html.window.location.href);
-          }
-        }
-      },
-      child: RawGestureDetector(
-        gestures: <Type, GestureRecognizerFactory>{
-          HorizontalDragGestureRecognizer: GestureRecognizerFactoryWithHandlers<
-              HorizontalDragGestureRecognizer>(
-            () => HorizontalDragGestureRecognizer(),
-            (HorizontalDragGestureRecognizer instance) {
-              instance
-                ..onStart = (_) {
-                  html.window.history
-                      .pushState(null, '', html.window.location.href);
-                }
-                ..onUpdate = (_) {
-                  html.window.history
-                      .pushState(null, '', html.window.location.href);
-                };
-            },
-          ),
-        },
-        behavior: HitTestBehavior.opaque,
-        child: child,
-      ),
-    );
+    return child;
   }
-
-  //Required
-  bool shouldUpdateScrollBehavior(ScrollBehavior oldDelegate) => false;
 
   @override
   TargetPlatform getPlatform(BuildContext context) =>
